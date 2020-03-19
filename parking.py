14d1616253a2858d2f9e5500fda2b5b719dac652
besthl <60530352+besthl@users.noreply.github.com>
2020-03-19 14:21:03 -0400

    finally it can upload data to elasticsearch

--- parking.py
+++ parking.py
@@ -2,9 +2,8 @@ from sys import argv
 from src.OPCV.getdata import Function
 from os import environ
 from math import ceil
-
 from elasticsearch import Elasticsearch
-
+from sodapy import Socrata
 
 def create_and_update_index(index_name, doc_type):
     es = Elasticsearch()
@@ -12,20 +11,19 @@ def create_and_update_index(index_name, doc_type):
         es.indices.create(index=index_name)
     except Exception:
         pass
-
-    
     return es
 
 def insert(docs, es):
     for doc in docs:
         res = es.index(index='violation-parking-index', doc_type='vehicle', body=doc, )
-        print('Inserting ....')
+        #print('Inserting ....')
         print(res['result'])    
-        print('\n')
+        #print('\n')
 
 if __name__ == "__main__":
-    app_key = environ.get("APP_KEY")
-    #es = create_and_update_index('violation-parking-index', 'vehicle')
+    app_key = 'gOC80zDpmRj2bjooeYI5HMKVO'    
+
+    es = create_and_update_index('violation-parking-index', 'vehicle')
     #print page_size and num_pages
     page_size_str = argv[1]
     page_size = int(page_size_str.split('=')[1])
@@ -33,27 +31,19 @@ if __name__ == "__main__":
         num_pages_str = argv[2]
         num_pages = int(num_pages_str.split('=')[1])
     except Exception:
-        num_pages = None
+        num_pages = None  
+   
     
-    #print output
-    try:
-        output = argv[3]
-
-    except Exception:
-        output = None
+    es = Elasticsearch()
     location = 'nc67-uf89'
 
 
     # if the output is es, send data to Elasticsearch 
     with Function(app_key) as function:
-		
-        es = create_and_update_index('violation-parking-index', 'vehicle')
-
-        if num_pages == 0: 
-            total_size = function.get_size(location)
-            num_pages = ceil(total_size / page_size)
-				
-				# extract info
+        #es = create_and_update_index('violation-parking-index', 'vehicle')
+        if num_pages is None: 
+            	
+			# extract info
             docs = function.get_info(location, page_size)
             print('Extraction successful\n')
 				
@@ -64,25 +54,20 @@ if __name__ == "__main__":
 				
         else:
             total_size = function.get_size(location)
-		# print(f"total_size={total_size}")
-				
+            # print(f"total_size={total_size}")	
             docs = function.get_info(location, page_size)
             insert(docs, es)
-
-            for i in range(num_pages):       
+            for i in range(num_pages):    
                 docs = function.get_info(location, page_size, offset=i*num_pages)
-                print(docs)
-					
+                print(docs)	
                 print('Extraction successful\n')
-					# Load data
+			    # Load data
                 insert(docs, es)
                 print('Laoding successful\n')
-				
+
             print (f'num_pages = {num_pages}')
             print (f'page_size = {page_size}')
             print('Loading to ES')
             h = input("Press 'Enter' to continue...")				
 				
-				
-				
-	
+			
